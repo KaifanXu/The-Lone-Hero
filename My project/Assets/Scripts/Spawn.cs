2a6069db308d5ef538faf012dd0082637a56f7d3
Soda <xukaifan90@gmail.com>
2022-09-18 16:50:58 -0400

    added spawn point

---
+++ My project/Assets/Scripts/Spawn.cs
@@ -0,0 +1,43 @@
+﻿
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject Prefab;
+
+
+    //public float spawnDelayMin;
+    //public float spawnDelayMax;
+    public int enemyNum;
+    public float spawnTime;
+    public float spawnDelay;
+
+    private float spawnTimer;
+    private Transform spawnPoint;
+
+    private void Awake()
+    {
+        //spawnTimer = Random.Range(spawnDelayMin, spawnDelayMax);
+        spawnTimer = spawnTime;
+        spawnPoint = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0 && enemyNum > 0)
+        {
+            Instantiate(Prefab, spawnPoint.position, spawnPoint.rotation);
+            //spawnTimer = Random.Range(spawnDelayMin, spawnDelayMax);
+            spawnTimer = spawnTime + spawnDelay;
+
+            enemyNum--;
+        }
+        if (enemyNum == 0)
+            Destroy(this.gameObject);
+    }
+
+
+}
