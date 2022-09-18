b9e2a9e3a86ab1edfb8b4a575e4d54e317010b0f
Soda <xukaifan90@gmail.com>
2022-09-18 15:27:32 -0400

    First

---
+++ My project/Assets/Scripts/EnemyPath.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyPath : MonoBehaviour
+{
+
+    public NavMeshAgent enemy;
+    public Transform playerBase;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemy.SetDestination(playerBase.position);
+    }
+}
---
+++ My project/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 0.1f;
+    // Update is called once per frame
+    void Update()
+    {
+        float movingX = Input.GetAxis("Horizontal");
+        float movingY = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(movingX, 0.0f, movingY);
+
+        transform.position += movement * speed;
+    }
+}
